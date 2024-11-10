904c9030cecfbcc99c0003b72632aecc90df32de
Zlatin <mzlatin4@gmail.com>
2024-11-10 11:06:06 +0100

    server.socket.py
    I made the code understandable and cleaned up messes.

--- server.socket.py
+++ server.socket.py
@@ -1,27 +1,27 @@
-import asyncio
-import websockets
+import asyncio #importiert asyncio
+import websockets#importiert weboskets
 
-clients = []
+clients = [] #hier werden alle lezte gespeichert die neu joinen
 
-async def handle_client(websocket, path):
-    clients.append(websocket)
-    print("New client connected")
+async def handle_client(websocket, path): #code welcher sich um leute kümmert die neu dazu joinen
+    clients.append(websocket) #fügt den client hinzu
+    print("New client connected") # ansage das jemand neu dauz gekommen ist
 
     try:
-        async for message in websocket:
+        async for message in websocket: 
             print(f"Received: {message}")
             # Weiterleiten der Nachricht an alle anderen Clients
             for client in clients:
-                if client != websocket:
+                #if client != websocket:
                     await client.send(message)  # Sende die Nachricht weiter
-    except websockets.exceptions.ConnectionClosed:
-        print("Client disconnected")
+    except websockets.exceptions.ConnectionClosed: # client disconectet
+        print("Client disconnected") # ansage
     finally:
-        clients.remove(websocket)
+        clients.remove(websocket) # remove client
 
-async def main():
-    server = await websockets.serve(handle_client, "Put your ip and port here") #Put your ip and port here
-    print("Server started on your ip and port")
+async def main(): #server starten def
+    server = await websockets.serve(handle_client, "127.0.0.1", 5555) #meine ip 
+    print("Server started on ws://127.0.0.1:5555") # ansage
     await server.wait_closed()
 
-asyncio.run(main())
+asyncio.run(main()) #sever starten wurde in line 22-25 beschriben
